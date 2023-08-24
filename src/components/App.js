430fea9e009787ff7f8f2c42e84d7b33449361c2
Spencer <eklund.spencer@outlook.com>
2023-08-24 13:40:46 -0600

    passed props through components

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name={user.name} city={user.city}/>
+      <About bio="Here's some info about me" github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
